de3342f98125fe0707e2b738c9ce708b71386405
”Arthur <arthur.maydanuk@gmail.com>
2021-02-26 01:30:22 +0200

    feat: complete all of tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) n = -n;
+    let arr = [];
+    n = String(n);
+    for(let i = n.length; i>=0; i-- ) {
+        arr.push(n[i]);
+    }
+    return Number(arr.join(''))
 }
